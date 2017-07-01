2bc7385af9d951e56e8e60e9520587f6e1837526
Oak Kim <solidoak.ninja@gmail.com>
2017-07-01 12:57:58 -0400

    'Create a new project' feature complete.

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,11 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+  root "projects#index"
+
+  resources :projects
+
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
